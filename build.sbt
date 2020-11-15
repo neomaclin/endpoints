72ec3fc85c382e250eafde5b673c3506e2a4ffe9
Neo Lin <Neo Lin>
2020-11-15 18:13:57 -0500

    Recent Cleanups

--- build.sbt
+++ build.sbt
@@ -8,7 +8,7 @@ scalaVersion := "2.13.2"
 libraryDependencies ++= webStack ++ crypto ++ stackGlue
 
 lazy val webStack = circe ++ sttp ++ jwt
-lazy val stackGlue = enumlib ++ macwire
+lazy val stackGlue = enumlib
 lazy val crypto = bc
 
 lazy val bc = {
@@ -17,22 +17,12 @@ lazy val bc = {
 
 }
 
-lazy val macwire = {
-  val macWireVersion = "2.3.6"
-  Seq(
-    "com.softwaremill.macwire" %% "macros" % macWireVersion % Provided,
-    "com.softwaremill.macwire" %% "macrosakka" % macWireVersion % Provided,
-    "com.softwaremill.macwire" %% "util" % macWireVersion,
-    "com.softwaremill.macwire" %% "proxy" % macWireVersion
-  )
-}
-
 lazy val enumlib = {
   Seq(
     "com.beachape" %% "enumeratum",
     "com.beachape" %% "enumeratum-circe",
     "com.beachape" %% "enumeratum-quill"
-  ).map(_ %  "1.6.0")
+  ).map(_ % "1.6.0")
 }
 
 lazy val circe = {
@@ -45,19 +35,26 @@ lazy val circe = {
 }
 
 
-
-
 lazy val sttp = {
   Seq(
     "com.softwaremill.sttp.tapir" %% "tapir-json-circe",
     "com.softwaremill.sttp.tapir" %% "tapir-core",
     "com.softwaremill.sttp.tapir" %% "tapir-zio",
     "com.softwaremill.sttp.tapir" %% "tapir-akka-http-server",
-  ).map( _ % "0.16.15" ) ++
+    "com.softwaremill.sttp.tapir" %% "tapir-sttp-client",
+    "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs",
+    "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe-yaml"
+  ).map(_ % "0.16.15") ++
     Seq(
+      "com.softwaremill.sttp.model" %% "core" % "1.1.4",
       "com.softwaremill.sttp.client" %% "core" % "2.2.5",
-      "com.softwaremill.sttp.model" %% "core" % "1.1.4"
-    )
+    ) ++
+    Seq(
+      "com.softwaremill.sttp.client" %% "async-http-client-backend-zio",
+      "com.softwaremill.sttp.client" %% "async-http-client-backend-future",
+      "com.softwaremill.sttp.client" %% "akka-http-backend",
+      "com.softwaremill.sttp.client" %% "okhttp-backend"
+    ).map(_ % "2.2.9")
 }
 
 lazy val jwt = {
--- src/main/scala/com/quasigroup/inc/basic/package.scala
+++ src/main/scala/com/quasigroup/inc/basic/package.scala
@@ -1,19 +1,22 @@
 package com.quasigroup.inc
 
 import io.circe._
+import io.circe.generic.auto._
+import sttp.model._
 import sttp.tapir._
 import sttp.tapir.json.circe._
-import io.circe.generic.auto._
-import shapeless.HList
-import sttp.model.{QueryParams, StatusCode}
 
 
 package object basic {
+
   sealed trait ErrorInfo
 
   case class NotFound(what: String) extends ErrorInfo
+
   case class Unauthorized(realm: String) extends ErrorInfo
+
   case class Unknown(code: Int, msg: String) extends ErrorInfo
+
   case object NoContent extends ErrorInfo
 
   // here we are defining an error output, but the same can be done for regular outputs
@@ -25,11 +28,12 @@ package object basic {
       statusDefaultMapping(jsonBody[Unknown].description("unknown"))
     )
   )
-  val origins:EndpointInput[(Int, Option[Int])] =
+
+  val origins: EndpointInput[(Int, Option[Int])] =
     query[Int]("from") and query[Option[Int]]("with")
 
-  def pagination(pageNumber:String, pageSize:String): EndpointInput[(Int, Int)] =
-    query[Int](name = pageNumber) and query[Int]( name = pageSize)
+  def pagination(pageNumber: String, pageSize: String): EndpointInput[(Int, Int)] =
+    query[Int](name = pageNumber) and query[Int](name = pageSize)
 
   object Heartbeat {
     def ping(path: String): Endpoint[Unit, Unit, Unit, Nothing] =
@@ -38,7 +42,7 @@ package object basic {
 
   object Entrance {
 
-    def indexJson[T:Encoder: Decoder:Schema:Validator]: Endpoint[Unit, Unit, T, Nothing] =
+    def indexJson[T: Encoder : Decoder : Schema : Validator]: Endpoint[Unit, Unit, T, Nothing] =
       endpoint.get
         .in("index")
         .out(jsonBody[T]).out(statusCode(StatusCode.Ok))
@@ -50,47 +54,53 @@ package object basic {
 
   }
 
-  object Authentication{
+  object Authentication {
 
-    def signIn[T:Encoder: Decoder:Schema:Validator, R:Encoder: Decoder:Schema:Validator](signInPath:String): Endpoint[(Int, Option[Int], Option[T]), Unit, R, Nothing] =
+    def signIn[T: Encoder : Decoder : Schema : Validator, R: Encoder : Decoder : Schema : Validator](signInPath: String): Endpoint[(Int, Option[Int], Option[T]), Unit, R, Nothing] =
       endpoint.post
         .in(signInPath)
         .in(origins)
         .in(jsonBody[Option[T]])
         .out(jsonBody[R]).out(statusCode(StatusCode.Ok))
 
-    def signOut[R:Encoder: Decoder:Schema:Validator](signOutPath:String): Endpoint[Unit, Unit, R, Nothing] = endpoint
+    def signOut[R: Encoder : Decoder : Schema : Validator](signOutPath: String): Endpoint[Unit, Unit, R, Nothing] = endpoint
       .delete
       .in(signOutPath)
       .out(jsonBody[R])
       .out(statusCode(StatusCode.Ok))
   }
 
-  object Content{
+  object Content {
 
-    def post[T:Encoder: Decoder:Schema:Validator](path:String): Endpoint[T, Unit, Unit, Nothing] =
+    def post[T: Encoder : Decoder : Schema : Validator](path: String): Endpoint[T, Unit, Unit, Nothing] =
       endpoint.post.in(path).in(jsonBody[T])
         .out(statusCode(StatusCode.Created))
         .errorOut(statusCode(StatusCode.Conflict))
 
-    def getCollectionOf[T:Encoder: Decoder:Schema:Validator](path: String): Endpoint[Unit, Unit, List[T], Nothing] =
+    def getCollectionOf[T: Encoder : Decoder : Schema : Validator](path: String): Endpoint[Unit, Unit, List[T], Nothing] =
       endpoint.get.in(path)
         .out(jsonBody[List[T]]).out(statusCode(StatusCode.Ok))
         .errorOut(statusCode(StatusCode.NotFound))
 
-    def getCollectionByParameters[R:Encoder: Decoder:Schema:Validator](path: String): Endpoint[QueryParams, Unit, List[R], Nothing] =
+    def getCollectionByParameters[R: Encoder : Decoder : Schema : Validator](path: String): Endpoint[QueryParams, Unit, List[R], Nothing] =
       endpoint.get.in(path).in(queryParams)
         .out(jsonBody[List[R]]).out(statusCode(StatusCode.Ok))
         .errorOut(statusCode(StatusCode.NotFound))
 
-    def getCollectionByBody[Q:Encoder: Decoder:Schema:Validator, R:Encoder: Decoder:Schema:Validator](path: String): Endpoint[Q, Unit, List[R], Nothing] =
+    def getCollectionByBody[Q: Encoder : Decoder : Schema : Validator, R: Encoder : Decoder : Schema : Validator](path: String): Endpoint[Q, Unit, List[R], Nothing] =
       endpoint.get.in(path).in(jsonBody[Q])
         .out(jsonBody[List[R]]).out(statusCode(StatusCode.Ok))
         .errorOut(statusCode(StatusCode.NotFound))
 
-    def update[T:Encoder: Decoder:Schema:Validator](path:String): Endpoint[T, Unit, Unit, Nothing] =
+    def update[T: Encoder : Decoder : Schema : Validator](path: String): Endpoint[T, Unit, Unit, Nothing] =
       endpoint.put.in(path).in(jsonBody[T])
         .out(statusCode(StatusCode.Accepted))
         .errorOut(statusCode(StatusCode.Conflict))
   }
+
+  object Session{
+
+  }
+
+
 }
